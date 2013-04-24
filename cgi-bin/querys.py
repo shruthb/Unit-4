2f93aff6279bcf39c1370d4071f3e295a78aca6c
10pics092107 <supriyakathare@yahoo.co.in>
2013-04-24 23:26:20 +0530

    Create querys.py

---
+++ cgi-bin/querys.py
@@ -0,0 +1,42 @@
+import cgi, threading
+from core import webcrawl_class
+from pymongo import Connection
+
+connection1=Connection()    #making a connection
+db1=connection1.url_database1  #getting a database
+newcollection=db1.url_newcollection #getting a collection
+
+print "Content-type: text.html\r\n\r\n"
+print ""
+print "<html><head><title>"
+print "Output</title></head><body>"
+
+#removes the unicode format
+def formatres(resdict):
+    del resdict[u'_id']
+    new={}
+    for each in resdict.keys():
+        new[str(each)]=str(resdict[each])
+    return new
+
+form = cgi.FieldStorage()
+url = form.getvalue("url")
+res = newcollection.find_one({'url': url})
+
+if not res :
+   print "<h2>We haven't crawled ",url,"yet!</h2>"
+   print "<div> Please get back later </div>"
+   mon.needtocrawl(url)
+   
+else :
+   print 'result!!!!!'
+   #print res
+   ans=formatres(res)
+   if ans['mobcomp']=='True':
+       print '%s is mobile compatible'%(url)
+   else:
+       print '%s is not mobile compatible'%(url)
+
+
+print "</body></html>"
+     
